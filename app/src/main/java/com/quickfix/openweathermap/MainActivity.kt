1bda077ccaa60735015416e089a90888b9d95f6b
Zain Naqvi <Zain.N@devnatives.com>
2023-06-12 17:38:32 +0500

    Weather application is completed

--- app/src/main/java/com/quickfix/openweathermap/MainActivity.kt
+++ app/src/main/java/com/quickfix/openweathermap/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import kotlin.math.ceil
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,15 +24,36 @@ class MainActivity : AppCompatActivity() {
 
     private fun getJsonData( lat:String, lng:String ){
         val queue = Volley.newRequestQueue(this)
-        val apiKey = "e1816280a64ae2d04fc39692c5a87354"
+        val apiKey = "45bb8427b1a339565af6ebe2468589d2"
         val url = "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}"
 
        val jsonData = JsonObjectRequest(Request.Method.GET, url , null, { response ->
-           Toast.makeText(this, response.toString(), Toast.LENGTH_LONG).show()
+        setValues(response)
        }, {
            Toast.makeText(this, "Internal server error!", Toast.LENGTH_LONG).show()
        })
 
        queue.add(jsonData)
+    }
+  private fun  setValues(response: JSONObject){
+        cityName.text=response.getString("name")
+        var lat = response.getJSONObject("coord").getString("lat")
+        var lng = response.getJSONObject("coord").getString("lon")
+        coordinates.text="latitude - $lat, longitude - $lng"
+        weather.text = response.getJSONArray("weather").getJSONObject(0).getString("main")
+        var tempr = response.getJSONObject("main").getString("temp")
+        tempr = (((tempr).toFloat()-273.15)).toInt().toString()
+        tempText.text = tempr + "°C"
+        var min_tempr = response.getJSONObject("main").getString("temp_min")
+        min_tempr=(((min_tempr).toFloat()-273.15)).toInt().toString()
+        minTemp.text = min_tempr+ "°C"
+        var max_tempr = response.getJSONObject("main").getString("temp_max")
+        max_tempr=((ceil((max_tempr).toFloat()-273.15))).toInt().toString()
+        maxTemp.text = max_tempr+ "°C"
+        pressure.text = response.getJSONObject("main").getString("pressure")
+        humidity.text = response.getJSONObject("main").getString("humidity")
+        windSpeed.text ="Wind Speed - " + response.getJSONObject("wind").getString("speed")
+        degree.text ="Degree - " + response.getJSONObject("wind").getString("deg")
+        gust.text ="Gust - " + response.getJSONObject("wind").getString("gust")
     }
 }
